aceedd0a87db56458fb7d15e4ed3c70f092d7a02
Detelina Palova <palova.detelina@gmail.com>
2023-08-24 22:29:22 +0300

    update server to use environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { initializeDatabase } = require('./db/db');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.BACKEND_PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
