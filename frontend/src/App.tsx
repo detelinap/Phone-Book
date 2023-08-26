6e828f4aad8b50c1e5224a6e417742e91e221b92
Detelina Palova <palova.detelina@gmail.com>
2023-08-26 20:50:56 +0300

    update AddNewEntryPage direction

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
-import AddNewEntryPage from './pages/AddNewEntryPage';
+import AddNewEntryPage from './pages/AddNewEntryPage/AddNewEntryPage';
 import EditEntryPage from './pages/EditEntryPage';
 
 const App: React.FC = () => {
